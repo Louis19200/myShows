5f58d0c73807279d1dff3bf7a69b94b998c832ec
Louis19200 <louischanteloup19@gmail.com>
2025-12-12 15:12:54 +0100

    exo 4

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DataService, DataSerieModel } from '../services/data.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +8,22 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
+  private data = inject(DataService);
+  selectedSegment = 'all';
 
   constructor() {}
 
+  getFilteredSeries(): DataSerieModel[] {
+    const series = this.data.getDataSeries();
+    if (this.selectedSegment === 'toWatch') {
+      return series.filter(s => s.seasonToSee > 0);
+    } else if (this.selectedSegment === 'finished') {
+      return series.filter(s => s.seasonToSee === 0);
+    }
+    return series;
+  }
+
+  onSegmentChange(event: any) {
+    this.selectedSegment = event.detail.value;
+  }
 }
